8136fdb9f154753954608ac8b9ec115fa8cbc348
OctoMiao <emptymalei@qq.com>
2018-03-01 20:38:06 -0700

    update progress

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # cities
 
 webpages for cities in the sky
+
+For development
+
+```
+npm run dev
+```
+
+For production
+
+```
+npm run build
+```
--- src/components/dashboard/data-visualisation-tab/DataVisualisation.vue
+++ src/components/dashboard/data-visualisation-tab/DataVisualisation.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-md-12 d-flex">
          <vuestic-widget headerText="南河城建设" class="city-card-widget">
-           <vuestic-city-card :name="'南河城'" :location="'火星，伊希地'" :progressValue="5" :cityInfo="'南河城是火星一号城，是星际移民中心的核心所在，同时也有很多服务型的企业落户于此。目前主干道已经施工完毕，城市中心区域的辅路和建筑正在建设中。'"
+           <vuestic-city-card :name="'南河城'" :location="'火星，伊希地'" :progressValue="20" :cityInfo="'南河城是火星一号城，是星际移民中心的核心所在，同时也有很多服务型的企业落户于此。目前主干道已经施工完毕，城市中心区域的辅路和建筑正在建设中。'"
                                  :reference="{github: 'github.com/interimm/martian-cities',book:'book.interimm.org',model:'3dwarehouse.sketchup.com/model/8d548832-b6ca-4f43-8a68-8048369b14b7/Isidis-City-Procyon'}">
            </vuestic-city-card>
          </vuestic-widget>
